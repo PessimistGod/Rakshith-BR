21ea8e156371c98017c2cc4493ab4fc768b009de
PessimistGod <rameshrahul0004@gmail.com>
2023-12-17 22:49:45 +0530

    Navbar Update

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaRegHeart } from "react-icons/fa6";
+import { BsHandbagFill } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [data, setData] = useState({ user_name: "Rahul" });
+  const [navbarData, setNavbarData] = useState({
+    office: {
+      Top: '/top',
+      Tunic: '/tunic',
+      Skirt: '/skirt',
+      Shirt: '/shirt',
+      Plazzo: '/plazzo',
+      Pant: '/pant',
+      Kurta: '/kurta',
+      Jacket: '/jacket',
+    },
+    bridal: {
+      Top: '/top',
+      Tunic: '/tunic',
+      Skirt: '/skirt',
+      Shirt: '/shirt',
+    },
+    ethnic: {
+      Top: '/top',
+      Tunic: '/tunic',
+      Skirt: '/skirt',
+      Shirt: '/shirt',
+      Plazzo: '/plazzo',
+      Pant: '/pant',
+      Kurta: '/kurta',
+      Jacket: '/jacket',
+    }
+  });
+  const [displayedLinks, setDisplayedLinks] = useState([]);
+  
+  function handleDisplay(category) {
+    const categoryData = navbarData[category];
+    if (categoryData) {
+      setDisplayedLinks(Object.entries(categoryData));
+    } else {
+      setDisplayedLinks([]);
+    }
+  }
 
-export default Navbar
+  function handleMouseOut() {
+    setDisplayedLinks([]);
+  }
+  
+  return (
+    <>
+      <nav className="flex justify-around bg-black text-white items-center py-3 relative">
+        <div className="flex space-x-2">
+          <div>
+            <img src="" alt="JANI-LOGO" />
+          </div>
+          <div>JANI</div>
+        </div>
+        <div className="flex space-x-8 items-center" onMouseLeave={handleMouseOut}>
+          <div onMouseEnter={() => handleDisplay('office')}>Office</div>
+          <div onMouseEnter={() => handleDisplay('bridal')}>Bridal</div>
+          <div onMouseEnter={() => handleDisplay('ethnic')}>Ethnic</div>
+        </div>
+        <div className="flex space-x-6 items-center">
+          <div>
+            <FaRegHeart size={22} />
+          </div>
+          <div>
+            <BsHandbagFill size={22} />
+          </div>
+          <div className="flex items-center space-x-1">
+            <div>{data.user_name}</div>
+            <CgProfile size={22} />
+          </div>
+        </div>
+      </nav>
+      {displayedLinks.length > 0 && (
+        <div className="h-[90vh] w-full bg-white flex flex-col absolute" onClick={handleMouseOut}>
+          {displayedLinks.map(([item, link]) => (
+            <Link key={item} to={link}>
+              {item}
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
   return (
     <>
     <div className='max-w-[99%] mx-auto my-1'>
-    <img className='w-full aspect-square md:aspect-auto' src="./MicrosoftTeams-image_13.jfif" alt="" />
+    {/* <img className='w-full aspect-square md:aspect-auto' src="./MicrosoftTeams-image_13.jfif" alt="" /> */}
     </div>
       <TextSection />
-      <CardElement />
+      {/* <CardElement /> */}
     </>
   )
 }
